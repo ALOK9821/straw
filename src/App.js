1f15740dbf16a795a8414baac2af8a2fb98e7a7b
Vishal Yadav <vy34365@gmail.com>
2021-07-29 14:24:12 +0530

    'navbar1'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import Header from "./Assets/Header.js";
 
 
 function App() {
   return (
     <>
-          <h1>new project initialized</h1>
-          <h2>Got it! Vishal Here</h2>
+          {/* <h1>new project initialized</h1>
+          <h2>Got it! Vishal Here</h2> */}
+          <Header />
     </>
   );
 }
